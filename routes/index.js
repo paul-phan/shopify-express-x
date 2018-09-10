4abab150c6478faa823a3b6e4da3f18ec8bd6681
Paul Phan <phanminh65@gmail.com>
2018-09-10 18:30:53 +0700

    Add Shopify GraphQL proxy

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
 
 const createShopifyAuthRoutes = require('./shopifyAuth');
 const shopifyApiProxy = require('./shopifyApiProxy');
+const shopifyGraphQLProxy = require('./shopifyGraphQLProxy');
 
 module.exports = function createRouter(shopifyConfig) {
-  const router = express.Router();
-  const rawParser = bodyParser.raw({ type: '*/*' });
-  const simpleCookieParser = cookieParser();
-  const {auth, callback, enableCookies} = createShopifyAuthRoutes(shopifyConfig)
+    const router = express.Router();
+    const rawParser = bodyParser.raw({ type: '*/*' });
+    const {auth, callback} = createShopifyAuthRoutes(shopifyConfig)
 
-  router.use('/auth/callback', callback);
-  router.use('/auth/enable_cookies', enableCookies);
-  router.use('/auth', simpleCookieParser, auth);
-  router.use(
-    '/api',
-    rawParser,
-    verifyApiCall,
-    shopifyApiProxy,
-  );
+    router.use('/auth/callback', callback);
+    router.use('/auth', auth);
+    router.use(
+        '/api',
+        rawParser,
+        verifyApiCall,
+        shopifyApiProxy,
+    );
+    router.use(shopifyGraphQLProxy());
 
-  return router;
+    return router;
 };
 
 function verifyApiCall(request, response, next) {
-  const {session} = request;
+    const {session} = request;
 
-  if (session && session.accessToken) {
-    next();
+    if (session && session.accessToken) {
+        next();
+        return;
+    }
+
+    response.status(401).send();
     return;
-  }
-
-  response.status(401).send();
-  return;
 }
