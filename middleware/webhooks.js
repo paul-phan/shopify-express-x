71402ab55b1e0c2d48b015f9c21c9a06d2df4747
Paul Phan <phanminh65@gmail.com>
2019-01-07 16:57:14 +0700

    manually return result for webhook

--- middleware/webhooks.js
+++ middleware/webhooks.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto');
 const getRawBody = require('raw-body');
 
-module.exports = function configureWithWebhook({ secret, shopStore }) {
+module.exports = function configureWithWebhook({secret, shopStore}) {
   return function createWebhookHandler(onVerified) {
     return async function withWebhook(request, response, next) {
-      const { body: data } = request;
+      const {body: data} = request;
       const hmac = request.get('X-Shopify-Hmac-Sha256');
       const topic = request.get('X-Shopify-Topic');
       const shopDomain = request.get('X-Shopify-Shop-Domain');
@@ -17,22 +17,20 @@ module.exports = function configureWithWebhook({ secret, shopStore }) {
           .digest('base64');
 
         if (generated_hash !== hmac) {
-          response.status(401).send();
           onVerified(new Error("Unable to verify request HMAC"));
           return;
         }
 
-        const {accessToken} = await shopStore.getShop({ shop: shopDomain });
+        const {accessToken} = await shopStore.getShop({shop: shopDomain});
 
         request.body = rawBody.toString('utf8');
-        request.webhook = { topic, shopDomain, accessToken };
+        request.webhook = {topic, shopDomain, accessToken};
 
-        response.status(200).send();
 
         onVerified(null, request);
       } catch (error) {
-        response.status(401).send();
         onVerified(new Error("Unable to verify request HMAC"));
+        request.webhook = {}
       } finally {
         next()
       }
