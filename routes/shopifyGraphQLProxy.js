8c4ef655acf1a00d56f2cfa535de8ca35726adf6
Paul <phanminh65@gmail.com>
2021-07-07 09:46:01 +0700

    update graphql proxy

--- routes/shopifyGraphQLProxy.js
+++ routes/shopifyGraphQLProxy.js
@@ -1,40 +1,26 @@
 const proxy = require('express-http-proxy');
 
-const PROXY_BASE_PATH = '/graphql';
 
-module.exports = function shopifyGraphQLProxy(shopifyConfig) {
-  const {apiVersion} = shopifyConfig
-  const GRAPHQL_PATH = `/admin/api/${apiVersion}/graphql.json`;
-
+module.exports = function shopifyGraphQLProxy(configs) {
+  const {apiVersion} = configs
   return function shopifyGraphQLProxyMiddleware(req, res, next) {
-    if (!req.session) {
-      console.error("A session middleware must be installed to use ApiProxy.");
-      res.status(401).send(new Error("Unauthorized"));
-      return;
-    }
     const { session: { shop, accessToken } } = req;
-
-    if (req.path !== PROXY_BASE_PATH || req.method !== "POST") {
-      return next();
-    }
-
     if (!accessToken || !shop) {
-      return res.status(403).send("Unauthorized");
+      return res.status(403).json({success: false, message: 'Unauthorized'});
     }
+    const GRAPHQL_PATH = `https://${shop}/admin/api/${apiVersion}/graphql.json`;
 
     proxy(shop, {
-            https: true,
-            parseReqBody: false,
-            proxyReqOptDecorator(proxyReqOpts, srcReq) {
-                proxyReqOpts.headers['content-type'] = 'application/json';
-                proxyReqOpts.headers['x-shopify-access-token'] = accessToken;
-                return proxyReqOpts;
-            },
-            proxyReqPathResolver(req) {
-                return GRAPHQL_PATH;
-            }
-        })(req, res, next);
-    }
+      https: true,
+      parseReqBody: false,
+      proxyReqOptDecorator(proxyReqOpts, srcReq) {
+        proxyReqOpts.headers['content-type'] = 'application/json';
+        proxyReqOpts.headers['x-shopify-access-token'] = accessToken;
+        return proxyReqOpts;
+      },
+      proxyReqPathResolver(req) {
+        return GRAPHQL_PATH;
+      }
+    })(req, res, next);
+  }
 };
-
-module.exports.PROXY_BASE_PATH = PROXY_BASE_PATH;
